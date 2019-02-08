bf8d854ed5a29409184992a8112ed737589f5406
raysr <raysamram@gmail.com>
2019-02-08 16:07:16 +0100

    adding the notebook and test script

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Gym-Deep-Q-Agents
 Practional jupyter notebooks for applying deep-q-learning on gym environments.
+Check the notebook for the analyzed script.
+And to test the environment and hyperparameters you want just :
+```
+python3.6 test-yourself.py
+```
+
+Then follow the instructions.
+
+## Dependencies :
+* **OpenAI Gym** : Providing all the environements. (and gym[atari] if you want atari games also).
+* **Keras** : For the Deep-Q Neural Network.
+
+```
+pip3 install keras gym gym[atari]
+```
---
+++ test-yourself.py
@@ -0,0 +1,104 @@
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+from collections import deque
+
+import random
+import numpy as np
+import gym
+
+import os
+
+all_envs = gym.envs.registry.all()
+env_ids = [env_spec.id for env_spec in all_envs]
+a=0
+for i in env_ids:
+    print(str(a)+" - "+str(i))
+    a+=1
+selected=int(input("Select the environment to learn (Number) : "))
+env = gym.make(env_ids[selected])
+
+# Create network. Input is two consecutive game states, output is Q-values of the possible moves.
+model = Sequential()
+model.add(Dense(20, input_shape=(2,) + env.observation_space.shape, init='uniform', activation='relu'))
+model.add(Flatten())       # Flatten input so as to have no problems with processing
+model.add(Dense(18, init='uniform', activation='relu'))
+model.add(Dense(10, init='uniform', activation='relu'))
+model.add(Dense(env.action_space.n, init='uniform', activation='linear'))    # Same number of outputs as possible actions
+
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+
+# Parameters
+D = deque()                                # Register where the actions will be stored
+
+observetime = int(input("Number of observe time ?"))                          # Number of timesteps we will be acting on the game and observing results
+epsilon = float(input("Probability of choosing random move ? "))                              # Probability of doing a random move
+gamma = float(input("Time factor ? (gamma)"))                                # Discounted future reward. How much we care about steps further in time
+mb_size = int(input( " Mini-batches size ? "))                               # Learning minibatch size
+
+
+# Beginning of the game
+observation = env.reset()
+obs = np.expand_dims(observation, axis=0)  
+state = np.stack((obs, obs), axis=1)
+done = False
+for i in range(observetime):
+    if (np.random.rand()<= epsilon): # Random factor
+        action = np.random.randint(0, env.action_space.n, size=1)[0] # Random move
+    else:
+        Q = model.predict(state) # Q-based move, which is predicted by the modal
+        action = np.argmax(Q) 
+    observation_new, reward, done, info = env.step(action) # Do the action and get the reward, state..ect.
+    obs_new = np.expand_dims(observation_new, axis=0)
+    state_new = np.append(np.expand_dims(obs_new, axis=0), state[:, :1, :], axis=1) 
+    D.append((state, action, reward, state_new, done)) 
+    state = state_new 
+    if done:
+        env.reset()           # Restart game at the end
+        obs = np.expand_dims(observation, axis=0)     
+        state = np.stack((obs, obs), axis=1)
+print("Observe time finished")
+
+
+
+
+minibatch = random.sample(D, mb_size)                              
+inputs_shape = (mb_size,) + state.shape[1:]
+inputs = np.zeros(inputs_shape)
+targets = np.zeros((mb_size, env.action_space.n))
+
+
+print("Learning phase")
+for i in range (0, mb_size):
+    state = minibatch[i][0]
+    action = minibatch[i][1]
+    reward = minibatch[i][2]
+    state_new = minibatch[i][3]
+    done = minibatch[i][4]
+    
+    inputs[i:i+1] = np.expand_dims(state, axis=0)
+    targets[i] = model.predict(state)
+    Q_sa = model.predict(state_new)
+    
+    if done:
+        targets[i, action] = reward
+    else:
+        targets[i, action] = reward + gamma * np.max(Q_sa)
+    model.train_on_batch(inputs, targets)
+
+
+    print("Play time")
+observation = env.reset()
+obs = np.expand_dims(observation, axis=0)
+state = np.stack((obs, obs), axis=1)
+done = False
+tot_reward = 0.0
+while not done:
+    env.render()                    
+    Q = model.predict(state)        
+    action = np.argmax(Q)         
+    observation, reward, done, info = env.step(action)
+    obs = np.expand_dims(observation, axis=0)
+    state = np.append(np.expand_dims(obs, axis=0), state[:, :1, :], axis=1)    
+    tot_reward += reward
+print("End of the game")
