2c08c06d72c9d80c9952f1f145ffe2c567194123
raysr <raysamram@gmail.com>
2019-02-12 20:36:42 +0100

    optimizing learning

--- play.py
+++ play.py
@@ -13,26 +13,17 @@ all_envs = gym.envs.registry.all()
 env_ids = [env_spec.id for env_spec in all_envs]
 a=0
 selected=494
-env = gym.make(env_ids[selected])
+env = gym.make("Breakout-ram-v0")
 
 # Create network. Input is two consecutive game states, output is Q-values of the possible moves.
 
-model = Sequential()
-model.add(Dense(20, input_shape=(2,) + env.observation_space.shape, init='uniform', activation='relu'))
-model.add(Flatten())       # Flatten input so as to have no problems with processing
-model.add(Dense(18, init='uniform', activation='relu'))
-model.add(Dense(18, init='uniform', activation='relu'))
-model.add(Dense(18, init='uniform', activation='relu'))
-model.add(Dense(10, init='uniform', activation='relu'))
-model.add(Dense(env.action_space.n, init='uniform', activation='linear'))    # Same number of outputs as possible actions
-'''
-    print("Loading model")
-    json_file = open('model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    # load weights into new model
-'''
+print("Loading model")
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+
+
 model.load_weights("model_weights.h5")
 print("Loaded model from disk")
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
