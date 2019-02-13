5a8683833e544daeb62145f274446ff471675743
raysr <raysamram@gmail.com>
2019-02-13 14:58:13 +0100

    new model update

--- test-yourself.py
+++ test-yourself.py
@@ -1,107 +1,112 @@
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
-from collections import deque
-from keras.models import model_from_json
+# INITIALIZATION: libraries, parameters, network...
+
+from keras.models import Sequential      # One layer after the other
+from keras.layers import Dense, Flatten  # Dense layers are fully connected layers, Flatten layers flatten out multidimensional inputs
+from collections import deque            # For storing moves 
 import os
-import random
 import numpy as np
-import gym
+import gym                                # To train our network
+env = gym.make('MountainCar-v0')          # Choose game (any in the gym should work)
 
+import random     # For sampling batches from the observations
 
-
-all_envs = gym.envs.registry.all()
-env_ids = [env_spec.id for env_spec in all_envs]
-a=0
-'''
-for i in env_ids:
-    print(str(a)+" - "+str(i))
-    a+=1
-'''
-selected=494
-env = gym.make("Breakout-ram-v0")
-print(" Training on : Breakout-ram-v0")
 
 # Create network. Input is two consecutive game states, output is Q-values of the possible moves.
-if not (os.path.exists('./model_weights.h5')):
-    model = Sequential()
-    model.add(Dense(20, input_shape=(2,) + env.observation_space.shape, init='uniform', activation='relu'))
-    model.add(Flatten())       # Flatten input so as to have no problems with processing
-    model.add(Dense(18, init='uniform', activation='relu'))
-    model.add(Dense(18, init='uniform', activation='relu'))
-    model.add(Dense(18, init='uniform', activation='relu'))
-    model.add(Dense(10, init='uniform', activation='relu'))
-    model.add(Dense(env.action_space.n, init='uniform', activation='linear'))    # Same number of outputs as possible actions
-else:
-    print("Loading model")
-    json_file = open('model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    # load weights into new model
-    model.load_weights("model_weights.h5")
-    print("Loaded model from disk")
+model = Sequential()
+model.add(Dense(20, input_shape=(2,) + env.observation_space.shape, init='uniform', activation='relu'))
+model.add(Flatten())       # Flatten input so as to have no problems with processing
+model.add(Dense(18, init='uniform', activation='relu'))
+model.add(Dense(10, init='uniform', activation='relu'))
+model.add(Dense(env.action_space.n, init='uniform', activation='linear'))    # Same number of outputs as possible actions
+
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
-
 # Parameters
-                              # Register where the actions will be stored
+D = deque()                                # Register where the actions will be stored
 
-observetime = 50                        # Number of timesteps we will be acting on the game and observing results
-epsilon = 0.9                            # Probability of doing a random move
-gamma = 0.9                              # Discounted future reward. How much we care about steps further in time
-mb_size = 50                              # Learning minibatch size
-steps=1000
-for prog in range(0,steps):
-  print("Step ("+str(prog)+"/"+str(steps)+")")
-  # Beginning of the game
-  observation = env.reset()
-  D = deque()  
-  obs = np.expand_dims(observation, axis=0)  
-  state = np.stack((obs, obs), axis=1)
-  done = False
-  for i in range(observetime):
-      if (np.random.rand()<= epsilon): # Random factor
-          action = np.random.randint(0, env.action_space.n, size=1)[0] # Random move
-      else:
-          Q = model.predict(state) # Q-based move, which is predicted by the modal
-          action = np.argmax(Q) 
-      observation_new, reward, done, info = env.step(action) # Do the action and get the reward, state..ect.
-      obs_new = np.expand_dims(observation_new, axis=0)
-      state_new = np.append(np.expand_dims(obs_new, axis=0), state[:, :1, :], axis=1) 
-      D.append((state, action, reward, state_new, done)) 
-      state = state_new 
-      if done:
-          env.reset()           # Restart game at the end
-          obs = np.expand_dims(observation, axis=0)     
-          state = np.stack((obs, obs), axis=1)
+observetime = 1000000                         # Number of timesteps we will be acting on the game and observing results
+epsilon = 0.7                              # Probability of doing a random move
+gamma = 0.9                                # Discounted future reward. How much we care about steps further in time
+mb_size = 50000                               # Learning minibatch size
 
+# FIRST STEP: Knowing what each action does (Observing)
 
-
-
-  minibatch = random.sample(D, mb_size)                              
-  inputs_shape = (mb_size,) + state.shape[1:]
-  inputs = np.zeros(inputs_shape)
-  targets = np.zeros((mb_size, env.action_space.n))
+observation = env.reset()                     # Game begins
+obs = np.expand_dims(observation, axis=0)     # (Formatting issues) Making the observation the first element of a batch of inputs 
+state = np.stack((obs, obs), axis=1)
+done = False
+for t in range(observetime):
+    os.system("clear")
+    print("Observation ("+str(t)+"/"+str(observetime)+")")
+    if np.random.rand() <= epsilon:
+        action = np.random.randint(0, env.action_space.n, size=1)[0]
+    else:
+        Q = model.predict(state)          # Q-values predictions
+        action = np.argmax(Q)             # Move with highest Q-value is the chosen one
+    observation_new, reward, done, info = env.step(action)     # See state of the game, reward... after performing the action
+    obs_new = np.expand_dims(observation_new, axis=0)          # (Formatting issues)
+    state_new = np.append(np.expand_dims(obs_new, axis=0), state[:, :1, :], axis=1)     # Update the input with the new state of the game
+    D.append((state, action, reward, state_new, done))         # 'Remember' action and consequence
+    state = state_new         # Update state
+    if done:
+        env.reset()           # Restart game if it's finished
+        obs = np.expand_dims(observation, axis=0)     # (Formatting issues) Making the observation the first element of a batch of inputs 
+        state = np.stack((obs, obs), axis=1)
+print('Observing Finished')
 
 
 
-  for i in range (0, mb_size):
-      state = minibatch[i][0]
-      action = minibatch[i][1]
-      reward = minibatch[i][2]
-      state_new = minibatch[i][3]
-      done = minibatch[i][4]
 
-      inputs[i:i+1] = np.expand_dims(state, axis=0)
-      targets[i] = model.predict(state)
-      Q_sa = model.predict(state_new)
 
-      if done:
-          targets[i, action] = reward
-      else:
-          targets[i, action] = reward + gamma * np.max(Q_sa)
-      model.train_on_batch(inputs, targets)
 
+
+minibatch = random.sample(D, mb_size)                              # Sample some moves
+
+inputs_shape = (mb_size,) + state.shape[1:]
+inputs = np.zeros(inputs_shape)
+targets = np.zeros((mb_size, env.action_space.n))
+
+for i in range(0, mb_size):
+    os.system("clear")
+    print("Batch ("+str(i)+"/"+str(mb_size)+")")
+    state = minibatch[i][0]
+    action = minibatch[i][1]
+    reward = minibatch[i][2]
+    state_new = minibatch[i][3]
+    done = minibatch[i][4]
+    
+# Build Bellman equation for the Q function
+    inputs[i:i+1] = np.expand_dims(state, axis=0)
+    targets[i] = model.predict(state)
+    Q_sa = model.predict(state_new)
+    
+    if done:
+        targets[i, action] = reward
+    else:
+        targets[i, action] = reward + gamma * np.max(Q_sa)
+
+# Train network to output the Q function
+    model.train_on_batch(inputs, targets)
+print('Learning Finished')
+
+
+
+
+
+observation = env.reset()
+obs = np.expand_dims(observation, axis=0)
+state = np.stack((obs, obs), axis=1)
+done = False
+tot_reward = 0.0
+while not done:
+    env.render()                    # Uncomment to see game running
+    Q = model.predict(state)        
+    action = np.argmax(Q)         
+    observation, reward, done, info = env.step(action)
+    obs = np.expand_dims(observation, axis=0)
+    state = np.append(np.expand_dims(obs, axis=0), state[:, :1, :], axis=1)    
+    tot_reward += reward
+print('Game ended! Total reward: {}'.format(reward))
 
 # serialize model to JSON
 model_json = model.to_json()
